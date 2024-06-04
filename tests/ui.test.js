393ed2f4b32f79cea5139cf7a29b615e34722b1b
Emine Selimovska <emi956@abv.bg>
2024-06-04 13:14:57 +0300

    Submit the Register Form with Empty Confirm Password Input Field

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -179,3 +179,19 @@ test('Submit the Register Form with Empty Password Input Field', async ({page}) 
    expect(page.url()).toBe('http://localhost:3000/register');
 
 });
+
+
+test('Submit the Register Form with Empty Confirm Password Input Field', async ({page}) => {
+    await page.goto('http://localhost:3000/register');
+    await page.fill('#email', 'sofi@abv.bg');
+    await page.fill('#password', '123456');
+    await page.click('#register-form > fieldset > input');
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('Alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    })
+    await page.$('a[href="/register"]');
+   expect(page.url()).toBe('http://localhost:3000/register');
+
+});
