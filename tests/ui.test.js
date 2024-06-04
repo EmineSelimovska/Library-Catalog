1572f97efb60538594573fd36e2e5f18451facbb
Emine Selimovska <emi956@abv.bg>
2024-06-04 10:47:25 +0300

    Verify Login Button is visible

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -8,4 +8,14 @@ test('Verify All books link is visible', async ({page}) => {
    const isLinkVisible = await allBooksLink.isVisible();
    expect(isLinkVisible).toBe(true);
 
+});
+
+
+test('Verify Login Button is visible', async ({page}) => {
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+   const loginButton = await page.$('a[href="/login"]');
+   const isLoginButtonVisible = await loginButton.isVisible();
+   expect(isLoginButtonVisible).toBe(true);
+
 })
