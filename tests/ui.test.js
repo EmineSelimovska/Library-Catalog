3355e7750c23b92df62a02da9322c35d7ea88bdb
Emine Selimovska <emi956@abv.bg>
2024-06-05 20:55:58 +0300

    Add book with empty URL fields

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -287,3 +287,29 @@ test('Add book with empty description fields', async ({page}) => {
 
 });
 
+
+test('Add book with empty URL fields', async ({page}) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('#email', 'peter@abv.bg');
+    await page.fill('#password', '123456');
+
+    await Promise.all([
+     page.click('input[type="submit"]'),
+     page.waitForURL('http://localhost:3000/catalog')
+    ]);
+    await page.click('a[href="/create"]');
+    await page.waitForSelector('#create-form');
+    await page.fill('#title', 'Test Book');
+    await page.fill('#description', 'This is a test book description');
+    await page.selectOption('#type', 'Fiction');
+   
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('Alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    })
+
+    await page.$('a[href="/create"]')
+     expect(page.url()).toBe('http://localhost:3000/create');
+
+});
