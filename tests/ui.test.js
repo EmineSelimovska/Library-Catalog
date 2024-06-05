c4a331e04385e24b3640e7fc9aa4589e39f15c0c
Emine Selimovska <emi956@abv.bg>
2024-06-05 22:18:34 +0300

    Verify That Logged-In User Sees Details Button and Button Works Correctly

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -336,3 +336,22 @@ test("Verify That No Books Are Displayed", async ({ page }) => {
   const noBookMessage = await page.textContent(".no-books");
   expect(noBookMessage).toBe("No Books in the Database!");
 });
+
+test("Verify That Logged-In User Sees Details Button and Button Works Correctly", async ({ page }) => {
+    await page.goto("http://localhost:3000/login");
+    await page.fill("#email", "peter@abv.bg");
+    await page.fill("#password", "123456");
+  
+    await Promise.all([
+      page.click('input[type="submit"]'),
+      page.waitForURL("http://localhost:3000/catalog"),
+    ]);
+    await page.click('a[href="/catalog"]')
+    await page.waitForSelector(".otherBooks");
+
+    await page.click('.otherBooks a.button')
+    await page.waitForSelector(".book-information");
+
+    const detailsPageTitle = await page.textContent(".book-information h3");
+    expect(detailsPageTitle).toBe("Test Book");
+  });
