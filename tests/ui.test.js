381a93cf1d903f51a909ef57581ea09c1fdf04b0
Emine Selimovska <emi956@abv.bg>
2024-06-13 22:21:56 +0300

    correction test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -342,10 +342,10 @@ test("Verify That No Books Are Displayed", async ({ page }) => {
 
 test("Verify That Logged-In User Sees Details Button and Button Works Correctly", async ({ page }) => {
     await page.goto("http://localhost:3000/login");
-    await page.fill("#email", "peter@abv.bg");
-    await page.fill("#password", "123456");
-  
-    await Promise.all([
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    
+   await Promise.all([
       page.click('input[type="submit"]'),
       page.waitForURL("http://localhost:3000/catalog"),
     ]);
@@ -358,22 +358,6 @@ test("Verify That Logged-In User Sees Details Button and Button Works Correctly"
     const detailsPageTitle = await page.textContent(".book-information h3");
     expect(detailsPageTitle).toBe("Test Book");
   });
-
-  
-test("Verify That Guest User Sees Details Button and Button Works Correctly", async ({ page }) => {
-    await page.goto("http://localhost:3000/login");
-   
-    await page.click('a[href="/catalog"]')
-    await page.waitForSelector(".otherBooks");
-
-    await page.click('.otherBooks a.button')
-    await page.waitForSelector(".book-information");
-
-    const detailsPageTitle = await page.textContent(".book-information h3");
-    expect(detailsPageTitle).toBe("Test Book");
-  });
-
-
 
 //   test("Verify That the logout button redirects correctly", async ({ page }) => {
 //     await page.goto("http://localhost:3000/login");
